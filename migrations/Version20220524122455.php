c48e7ec2817dad6d501c2b27047450f887c7ce8c
antho3105 <a.airaud.dev@gmail.com>
2022-06-03 12:33:09 +0200

    before merge from dev

---
+++ migrations/Version20220524122455.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220524122455 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE question ADD img_file_name VARCHAR(100) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE question DROP img_file_name');
+        $this->addSql('DROP INDEX UNIQ_8D93D649E7927C74 ON user');
+    }
+}
